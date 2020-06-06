150930aef6cd3cb7962f0688381c45518e81b5a9
BRUNO_REGES <bruno.reges@gmail.com>
2020-06-06 03:35:05 -0300

    implementando select UF

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker  } from 'react-leaflet'
+import axios from 'axios'
+import api from '../../services/api'
 
 import './styles.css'
 
 import logo from '../../assets/logo.svg'
 
+
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
+
+interface IBGEUFresponse {
+    sigla: string,
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs ] = useState<string[]> ([])
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            
+            setItems(response.data)
+        })
+    }, [])
+
+
+    useEffect(()=>{
+        axios.get<IBGEUFresponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response =>{
+            const ufInitials = response.data.map(uf=> uf.sigla)
+
+            setUfs(ufInitials)
+                
+            })
+        },[])
+
     return (
         <div id="page-create-point">
             <header>
@@ -61,11 +96,24 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-3.8080534,-38.5165226]} zoom={15}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position ={[-3.8080534,-38.5165226]} zoom={15}/>
+                    </Map>
+
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
                             <select name="uf" id="uf">
                             <option value="0">Selecione uma UF</option>
+                            {ufs.map(uf=>(
+                                <option key={uf} value={uf}>{uf}</option>
+                            ) )}
                             </select>
                             
                         </div>
@@ -86,7 +134,22 @@ const CreatePoint = () => {
                         <h2>Ítens de Coleta</h2>
                         <span>Selecione um ou mais ítens abaixo</span>
                     </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item => (
+                              <li key={item.id}>
+                              <img src={item.image_url} alt={item.title} />
+                            <span>{item.title}</span>
+                            </li>
+                        ))}
+                      
+                    </ul>
+
                 </fieldset>
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+
             </form>
 
         </div>
